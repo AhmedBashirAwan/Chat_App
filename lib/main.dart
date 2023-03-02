975e5a2870a0ed9f89f4d40a1bcfce7fcd0c3f02
Ahmed <ahmedbsahirawan@gmail.com>
2023-03-03 00:10:54 +0500

    asdfgh

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../common/routes/routes.dart';
-import '../../../common/theme/light_theme.dart';
-import '../../../feature/auth/controller/auth_controller.dart';
-import '../../../feature/home/pages/home_page.dart';
-import '../../../feature/welcome/pages/welcome_page.dart';
-import '../../../firebase_options.dart';
+import 'common/routes/routes.dart';
+import 'feature/auth/controller/auth_controller.dart';
+import 'feature/home/pages/home_page.dart';
+import 'feature/welcome/pages/welcome_page.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  try {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+  } catch (e) {
+    print(e);
+  }
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -29,24 +32,24 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Arno Chat',
-      theme: lightTheme(),
-      darkTheme: lightTheme(),
+      title: 'WhatsApp Me',
+      // theme: lightTheme(),
+      // darkTheme: darkTheme(),
       home: ref.watch(userInfoAuthProvider).when(
-            data: (user) {
-              FlutterNativeSplash.remove();
-              if (user == null) return const WelcomePage();
-              return const HomePage();
-            },
-            error: (error, trace) {
-              return const Scaffold(
-                body: Center(
-                  child: Text('Something wrong happened'),
-                ),
-              );
-            },
-            loading: () => const SizedBox(),
-          ),
+        data: (user) {
+          FlutterNativeSplash.remove();
+          if (user == null) return const WelcomePage();
+          return const HomePage();
+        },
+        error: (error, trace) {
+          return const Scaffold(
+            body: Center(
+              child: Text('Something wrong happened'),
+            ),
+          );
+        },
+        loading: () => const SizedBox(),
+      ),
       onGenerateRoute: Routes.onGenerateRoute,
     );
   }
