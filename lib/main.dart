e74bd8c4d086d486d1757a860ed81e229d379355
Ahmed <ahmedbsahirawan@gmail.com>
2023-03-05 17:39:13 +0500

    Bhai signned apk generate kr dain

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../../../common/routes/routes.dart';
 import '../../../common/theme/light_theme.dart';
 import '../../../feature/auth/controller/auth_controller.dart';
